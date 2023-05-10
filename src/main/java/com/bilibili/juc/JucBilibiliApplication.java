fcafc4b58062f20e33c87d96b2dc1df24d8bceef
hzhihuan <11654158@qq.com>
2023-05-10 23:58:36 +0800

    第一次

---
+++ src/main/java/com/bilibili/juc/JucBilibiliApplication.java
@@ -0,0 +1,22 @@
+package com.bilibili.juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// github 账户: 11654158@qq.com
+// github 密码: javacc++forgithub
+
+// github 账户: hwangzhihuan@gmail.com
+// github 密码: javacc++forgithub
+@SpringBootApplication
+public class JucBilibiliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JucBilibiliApplication.class, args);
+    }
+
+
+
+
+
+}
